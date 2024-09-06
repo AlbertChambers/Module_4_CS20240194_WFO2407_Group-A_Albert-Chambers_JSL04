48d9e5cb2ecc8ab3f1ebed87a6c4b28ed4b69af8
Albert Chambers <albertchambers09@gmail.com>
2024-09-06 14:14:41 +0200

    challenge 5 complete: final

--- index.js
+++ index.js
@@ -27,24 +27,10 @@ let currentTemp = 4;
 let serveCondition = currentTemp >= 5 ? "Chilled to perfection!" : "Needs a cooler!"
 
 console.log(serveCondition);
-debugger
-// #### Challenge 5: Late Night Coding Session
-// Determine if it's a good idea to have a Monster Energy drink based on the current hour.
-// If it's between 7am and midnight (24-hour format), print "Unleash the beast!",
-// otherwise, print "Better stick to water."
 
-// Define the current hour in 24-hour format
-let currentHour = 22; 
+// #### Challenge 5
+let currentHour = 22;
 
-// Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
-// If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
-// Otherwise, print "Better stick to water."
+let shouldDrinkMonster = currentHour >= 7 && currentHour < 24 ? "Unleash the beast!" : "Better stick to water.";
 
-
-
-
-
-
-// In each of these challenges, the ternary operator is used to decide between two options based on a condition, 
-// demonstrating its utility for concise conditional logic. Remember that while ternaries can make the code more succinct, 
-// they should be used judiciously to maintain readability, especially for more complex conditions.
+console.log(shouldDrinkMonster);
