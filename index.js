e325f262024833a4f5bb7aa550016bd9e8756d65
Albert Chambers <albertchambers09@gmail.com>
2024-09-06 13:45:02 +0200

    challenge 1 and 2

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// # [JSL04] Submission: Monster Ternary Operator
+// #### Challenge 1
+let userPreference ="";
+let preferredMonsterDrink = userPreference ? "Sugar-free" : "Regular";
 
-// #### Challenge 1: Flavor Selection
-// Given a variable that holds a user's preferred type of Monster Energy drink, 
-// print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
+console.log(`The user prefers: ${preferredMonsterDrink}`);
 
-let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+// #### Challenge 2
+let regularStock = 4;
+let sugarFreeStock = 6;
 
-// #### Challenge 2: Stock Check
-// You have a variable that tracks the number of Monster Energy cans left in the fridge. 
-// Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
+let regularStockStatus = regularStock < 5 ? "Time to restock regular Monster!" : "We're stocked on regular Monster.";
+let sugarFreeStockStatus = sugarFreeStock < 5 ? "Time to restock sugar-free Monster!" : "We're stocked on sugar-free Monster.";
 
-let cansLeft = 3; // Any number of cans
-console.log(``);
+console.log(regularStockStatus);
+console.log(sugarFreeStockStatus);
 
-
+debugger
 // #### Challenge 3: Workout Intensity
 // Based on the current heart rate, determine if the user should drink a Monster Energy drink to boost their workout. 
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
